5a7f61aa06fa8e3e3635dcf3c352c09f0d474226
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-08-23 22:54:45 -0300

    Acréscimo da opção de excluir cadastros.

--- projeto.js
+++ projeto.js
@@ -16,39 +16,79 @@
 
 nomes = []
 senhas = []
+nomesAux = []
+senhasAux = []
 index = parseInt(0)
 continuar = true
 
-while(continuar == true){
-    opcoes = prompt("O que deseja fazer? \n1 - Cadastrar usuário novo \n2 - Fazer login \n3 - Encerrar operação")
-    
+while (continuar == true) {
+    opcoes = prompt("O que deseja fazer? \n1 - Cadastrar usuário novo \n2 - Fazer login \n3 - Excluir cadastro \n4 - Encerrar operação")
+
     // Fazer cadastro
-    if(opcoes == 1){
-    nomes[index] = prompt("Insira o nome para cadastrar")
-    senhas[index] = prompt("Insira a senha para cadastrar")
-    console.log(nomes)
-    console.log(senhas)
-    index++
+    if (opcoes == 1) {
+        nomes[index] = prompt("Insira o nome para cadastrar")
+        senhas[index] = prompt("Insira a senha para cadastrar")
+        console.log(nomes)
+        console.log(senhas)
+        index++
     }
 
     // Fazer login
-    if(opcoes == 2){
+    if (opcoes == 2) {
         nomeLogin = prompt("Insira seu nome para logar")
         senhasLogin = prompt("Insira sua senha")
         login = false
-        for(index = 0; index < nomes.length; index++){
-            if(nomeLogin == nomes[index] && senhasLogin == senhas[index]){
+        for (index = 0; index < nomes.length; index++) {
+            if (nomeLogin == nomes[index] && senhasLogin == senhas[index]) {
                 console.log("Login efetuado.")
                 login = true
-            }else if(login == false){
+            } else if (login == false) {
                 console.log("Erro no login")
             }
         }
     }
-    // Encerra operação
-    if(opcoes == 3){
-        continuar = false
+    // Excluir cadastro
+    if (opcoes == 3) {
+        nomeExcluir = prompt("Qual o nome do cadastro para excluir?")
+        excluir = false
+        for (index = 0; index < nomes.length; index++) {
+            if (nomeExcluir == nomes[index]) {
+                console.log("Cadastro excluído.")
+                excluir = true
+                nomes[index] = 0
+                senhas[index] = 0
+            }
+        }
+        if (excluir == true) {
+            for (index = 0; index < nomes.length - 1; index++) {
+                for (seguinte = index + 1; seguinte < nomes.length; seguinte++) {
+                    if (nomes[index] == 0) {
+                        nomes[index] = nomes[seguinte]
+                        senhas[index] = senhas[seguinte]
+                        nomes[seguinte] = 0
+                        senhas[seguinte] = 0
+                    }
+                }
+            } for (index = 0; index < nomes.length; index++) {
+                if (nomes[index] != 0) {
+                    nomesAux[index] = nomes[index]
+                    senhasAux[index] = senhas[index]
+                }
+            }
+            nomes = nomesAux
+            senhas = senhasAux
+            nomesAux = []
+            senhasAux = []
+        }else if (exluir == false) {
+        console.log("Erro ao excluir cadastro.")
     }
 }
+    console.log(nomes)
+    console.log(senhas)
+    // Encerra operação
+    if (opcoes == 4) {
+        continuar = false
+    }
 
+}
 console.log("Programa encerrado.")
