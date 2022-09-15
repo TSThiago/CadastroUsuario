d318dff9e808b91c6b9caa8355ccb977cc995b68
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-09-15 20:56:15 -0300

    Criação da função que permite o usuário escolher a ação que deseja fazer.

--- projeto2.js
+++ projeto2.js
@@ -2,8 +2,30 @@ let nomes = []
 let senhas = []
 let parar = 0
 
-do {
-    nomes[nomes.length] = prompt("Insira o nome")
-    senhas[senhas.length] = prompt("Insira a senha")
-    parar = prompt("Insira 1 para parar")
-} while(parar != 1)
+// do {
+//     nomes[nomes.length] = prompt("Insira o nome")
+//     senhas[senhas.length] = prompt("Insira a senha")
+//     parar = prompt("Insira 1 para parar")
+// } while(parar != 1)
+
+EscolherAcao()
+
+function EscolherAcao(){
+    let opcao = 0
+    do{
+        opcao = prompt("O que deseja fazer? \n1 - Cadastrar usuário \n2 - Login \n3 - Encerrar programa")
+        switch(opcao){
+            case "1":
+                alert("Fazer cadastro:")
+                break;
+            case "2":
+                alert("Fazer login:")
+                break;
+            case "3":
+                alert("Programa encerrado.")
+                break;
+        }
+    }while(opcao != 3)
+}
+
+
