c17782e7b4ddd634938025b10468146af4e53317
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-09-15 20:12:24 -0300

    Criação de um algoritmo que preenche os arrays 'nomes' e 'senhas' e que para quando o usuário desejar.

--- projeto2.js
+++ projeto2.js
@@ -1,2 +1,9 @@
-nomes = []
-senhas = []
+let nomes = []
+let senhas = []
+let parar = 0
+
+do {
+    nomes[nomes.length] = prompt("Insira o nome")
+    senhas[senhas.length] = prompt("Insira a senha")
+    parar = prompt("Insira 1 para parar")
+} while(parar != 1)
