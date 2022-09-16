8bf5cce11f2393d845de4ca3993a82d6c5605d7d
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-09-15 23:10:58 -0300

    Criação da função que exclui o cadastro que o usuário deseja.

--- projeto2.js
+++ projeto2.js
@@ -1,13 +1,12 @@
-let nomes = ["Thiago","Luiz","Anderson"]
-let senhas = ["1","2","3"]
-let parar = 0
+let nomes = ["Thiago", "Luiz", "Anderson"]
+let senhas = ["1", "2", "3"]
 
 EscolherAcao()
 
 function EscolherAcao() {
     let opcao = 0
     do {
-        opcao = prompt("O que deseja fazer? \n1 - Cadastrar usuário \n2 - Login \n3 - Encerrar programa")
+        opcao = prompt("O que deseja fazer? \n1 - Cadastrar usuário \n2 - Login \n3 - Excluir Cadastro \n4 - Encerrar programa")
         switch (opcao) {
             case "1":
                 alert("Fazer cadastro:")
@@ -15,31 +14,67 @@ function EscolherAcao() {
                 break;
             case "2":
                 alert("Fazer login:")
-                FazerLogin()
+                FazerLogin(PedirNome(),PedirSenha())
                 break;
             case "3":
+                alert("Excluir cadastro:")
+                ExcluirCadastro(PedirNome())
+                break;
+            case "4":
                 alert("Programa encerrado.")
                 break;
+            default: 
+                alert("Opção inválida!")
         }
-    } while (opcao != 3)
+    } while (opcao != 4)
 }
 
-function FazerLogin() {
-    let nomeUsuario = prompt("Insira o nome")
-    let senhaUsuario = prompt("Insira a senha")
+function ExcluirCadastro(nomeParametro) {
     let mensagem = false
-    nomes.forEach((x , index) => {
-        if(x == nomeUsuario){
-            if(senhaUsuario == senhas[index]){
+    nomes.forEach((nome, index) => {
+        if (nomeParametro == nomes[index]) {
+            mensagem = true
+            nomes[index] = null
+            senhas[index] = null
+            let nomesAux = nomes.filter(x => x != null)
+            let senhasAux = senhas.filter(x => x != null)
+            nomes = nomesAux
+            senhas = senhasAux
+            console.log(nomes)
+            console.log(senhas)
+            return alert("Cadastro excluído com sucesso!")
+
+        }
+    })
+    if(!mensagem){
+    console.log(nomes)
+    console.log(senhas)
+    return alert("ERR0! Cadastro não encontrado.")
+}
+}
+
+function FazerLogin(nomeParametro,senhaParametro) {
+    let mensagem = false
+    nomes.forEach((x, index) => {
+        if (x == nomeParametro && senhaParametro == senhas[index]) {
                 mensagem = true
                 return alert("Login efetuado com sucesso!")
             }
-        }
     })
-    if(!mensagem)
-    return alert("Erro ao fazer o login.")
+    if (!mensagem)
+        return alert("Erro ao fazer o login.")
 }
-    function CadastrarUsuario() {
+
+function PedirNome(){
+    let nomeParametro = prompt("Insira o nome")
+    return nomeParametro
+}
+function PedirSenha(){
+    let senhaParametro = prompt("Insira a senha")
+    return senhaParametro
+}
+
+function CadastrarUsuario() {
     nomes[nomes.length] = prompt("Insira o nome")
     senhas[senhas.length] = prompt("Insira a senha")
 }
